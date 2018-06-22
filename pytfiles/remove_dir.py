e28179ad8803e48bc9edf500d0c4ff77faf5e7ed
Nicolas Boufidjeline <nicolas.boufidjeline@metronlab.com>
2018-06-22 16:30:35 +0200

    Correct something in remove_dir

--- pytfiles/remove_dir.py
+++ pytfiles/remove_dir.py
@@ -6,4 +6,4 @@ def remove_dir_func(dir_path):
     command = []
     command.append("rmdir")
     command.append(dir_path)
-    subprocess.run(command, subprocess.PIPE).stdout.decode('utf-8')
+    subprocess.run(command, subprocess.PIPE)
